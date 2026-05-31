d9d59f8fc5305faa924f4cfe9d7e76c319dd6344
Martins <obichukwumartins@gmail.com>
2026-05-31 20:19:10 +0100

    Second commit

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
+import requests
+response = requests.get("https://www.google.com")
+print(response.status_code)
+#print(response.text)
+print("hello, world")
+
+
 #Welcome message
-name= "Martins"
-age = 20
-print("Hello world!")
-print("I am learning python")
-print(name + " you are welcome to class, you are " +str(age) + " years old")
+
