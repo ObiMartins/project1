645f161b65bfc3a7eb8241f542a54fe333a13513
Martins <obichukwumartins@gmail.com>
2026-05-31 21:44:46 +0100

    Practical on Python variables committed

--- hello.py
+++ hello.py
@@ -7,3 +7,15 @@ print("hello, world")
 
 #Welcome message
 
+print("==========Student score===========")
+score = int(input("Enter your score"))
+if score >= 90:
+    print("Grade is: A")
+elif score >= 75:
+    print("Grade is: B")
+elif score >= 60:
+    print("Grade is: C") 
+elif score >= 50:
+    print("Grade is: D")
+else:
+    print("Grade is: F")
