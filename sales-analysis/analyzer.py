e74ea2630f5e86fb9fd3e7915d9988eb7dfe41d1
Martins <obichukwumartins@gmail.com>
2026-06-08 07:53:25 +0100

    Discount module added

--- sales-analysis/analyzer.py
+++ sales-analysis/analyzer.py
@@ -1,6 +1,6 @@
 # analyzer.py
 import pandas as pd
-from helpers import calculate_total, format_currency
+from helpers import calculate_total, format_currency, calculate_discount
 
 # Read data
 data_frame = pd.read_csv("data/sales.csv")
@@ -26,3 +26,9 @@ for index, row in data_frame.iterrows():
 grand_total = data_frame["total"].sum()
 #formatted_grand_total = format_currency(grand_total)
 print(f"\nGrand Total: {grand_total}")
+
+# Calculate Discount for Grand Total
+discount_price = calculate_discount(grand_total, 10)
+print(f"\nDiscount amount for Total price is : N{discount_price:.2f}")
+payable_amount = grand_total - discount_price
+print(f"\nPayable amount after discount is : N{payable_amount:.2f}")
