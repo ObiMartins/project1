8c4c7bf47cdddd094767b7bf30fb8f5db91e9487
Martins <obichukwumartins@gmail.com>
2026-06-10 19:49:44 +0100

    new commit

--- sales-analysis/helpers.py
+++ sales-analysis/helpers.py
@@ -5,7 +5,7 @@ def calculate_total(quantity, price):
 
 
 def format_currency(amount):
-    """Format number as currency."""
+    """Format number as currency of your choice."""
     return f"N{amount:,.2f}"
 
 def calculate_discount(price, discount):
